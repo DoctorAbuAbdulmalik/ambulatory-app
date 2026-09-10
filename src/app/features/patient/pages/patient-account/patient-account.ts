f46a4a28e29a63975068f0e5561bedf635117307
unknown <mr.betterdoctor@gmail.com>
2026-09-10 15:58:07 +0300

    feat(register):add step 3

---
+++ src/app/features/patient/pages/patient-account/patient-account.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-patient-account',
+  styleUrl: './patient-account.scss',
+  templateUrl: './patient-account.html',
+})
+export class PatientAccount {}
