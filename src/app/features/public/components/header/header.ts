eb8a3f1f06f8d16625f54639aa9435705fbea7a2
unknown <mr.betterdoctor@gmail.com>
2026-09-12 16:03:57 +0300

    feat: home page adaptive/responsive version

--- src/app/features/public/components/header/header.ts
+++ src/app/features/public/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   imports: [],
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   standalone: true,
 })
-export class Header {}
+export class Header {
+  private readonly isMenuOpenSignal = signal(false);
+  readonly isMenuOpen = this.isMenuOpenSignal.asReadonly();
+
+  toggleMenu(): void {
+    this.isMenuOpenSignal.update((open) => !open);
+  }
+
+  closeMenu(): void {
+    this.isMenuOpenSignal.set(false);
+  }
+}
